cf03e0b73ecc6461dbce1d7eb3fd05d8dcb0a3c0
Okafor Patrick Chibuike <patrickpeters911@gmail.com>
2023-02-16 06:47:50 -0800

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+
+	printf("with proper rammer, but the outcome is a piee of art,\n");
+	return (0);
+
+}
