64cb07a1c7c28e7561112c6ed94d945e50f9db4c
Okafor Patrick Chibuike <patrickpeters911@gmail.com>
2023-02-21 13:36:29 -0800

    a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
  *
  * Return Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 	char p[] = "_putchar";
 
-	for (i = 0; i < 7;i++)
-	{
-	_putchar(p[i]);
-	}
-	_putchar('\n');
-
+	for (i = 0; i < 8; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
 	return (0);
 }
