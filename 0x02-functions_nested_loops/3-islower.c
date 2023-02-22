654f5d596c029ac4535bb51f2dfc17f9bccb2eaa
Okafor Patrick Chibuike <patrickpeters911@gmail.com>
2023-02-21 16:51:52 -0800

    a function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,15 +4,15 @@
  *
  * Return: 1 for lowercase. 0 for the rest.
  */
-int _islower(int c);
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
 	return (1);
-	{
+	}
 	else
 	{
 	return (0);
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
