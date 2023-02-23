2fb4cf409551f4eacfdf7667ee19a02422ff8808
Okafor Patrick Chibuike <patrickpeters911@gmail.com>
2023-02-23 15:55:03 -0800

    a function that draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,29 @@
 #include "main.h"
 /**
-* print_diagonal - draws a diagonal line
-* @n: takes in an integer
-*/
+ * print_diagonal - Entry point
+ *
+ * @n: integer to represent
+ */
+
 void print_diagonal(int n)
 {
-	int i, j;
+int i;
+int j;
 
-	if (n <= 0)
-		_putchar('\n');
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j <= i; j++)
-		{
-			if (i == j)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-		}
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
 }
