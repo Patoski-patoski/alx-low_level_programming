b208d806a5c8524ce9ec05db977306ad64f8cac3
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-03 13:44:44 -0700

    a function to find the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
 		return (0);
-
-	_putchar(*s);
-
-	return (1 + _strlen_recursion(s + 1));
 }
