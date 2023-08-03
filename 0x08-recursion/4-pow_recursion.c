a69d1729f0587ea5d4fee5a7c87ba5ca0c458fd4
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-03 14:37:58 -0700

    that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /*
- * _pow_recursion - a function that returns the value of x raised to the power of y
- * @x: the value to return 
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y*
+ * @x: the value to return
  * @y: the exponential
  *
  * Return: 0 success
@@ -14,6 +15,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return x * _pow_recursion(x, (y - 1));
+	return (x * _pow_recursion(x, (y - 1)));
 }
 
