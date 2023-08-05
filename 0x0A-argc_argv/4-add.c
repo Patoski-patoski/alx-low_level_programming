48dea4696c77a22b38274b5604894f3ada9b9eb2
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-04 17:29:04 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -36,7 +36,10 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 			if (!isdigit(argv[i][j]))
+			{
+				puts("Error");
 				return (1);
+			}
 
 		sum += atoi(argv[i]);
 	}
