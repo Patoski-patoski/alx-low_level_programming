2cbfabe39e7eb9aa36b95f8dac1e5e66b78eb908
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-08 17:01:22 -0700

    a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,6 +9,6 @@ int findlength(char *str);
 char *str_concat(char *s1, char *s2);
 int mylength(char *str);
 int **alloc_grid(int width, int height);
-
+void free_grid(int **grid, int height);
 
 #endif
