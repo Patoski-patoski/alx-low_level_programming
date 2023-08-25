dc3434f5084c6dc83e01a36d67b7161d2c554f23
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-25 09:00:15 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s", 0
+	newline db 10, 0
+
+section .text 
+	global main
+	extern printf
+
+main:
+	push rdi ; Preserve the value of rdi register
+	lea rdi, [hello]
+	lea rsi, [format]
+	call printf
+
+	lea edi, [newline]
+	call printf
+
+	pop rdi ; Restore the value of rdi register
+	ret
