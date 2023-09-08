38b3caa82e8e39a403c5759819903b6df2162704
Okafor Patrick <patrickpeters911@gmail.com>
2023-09-08 09:18:19 -0700

    a program to create files and insert text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,10 @@ int create_file(const char *filename, char *text_content)
 	byte_written = write(fd, text_content, _strlen(text_content));
 
 	if (byte_written <= 0)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	return (1);
 }
