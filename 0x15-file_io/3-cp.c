b73d35ab8a18d8c72ef572ee615c4bab5d77d1db
Okafor Patrick <patrickpeters911@gmail.com>
2023-09-08 18:02:10 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -55,14 +55,11 @@ int copy_file(const char *file_from, const char *file_to)
 	{
 		byte_written = write(fd, buffer, byte_read);
 		if (byte_written == -1 || byte_written != byte_read)
-		{
 			return (-1);
-		}
 	}
 	if (byte_read <= 0)
-	{
 		return (-1);
-	}
+
 	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
