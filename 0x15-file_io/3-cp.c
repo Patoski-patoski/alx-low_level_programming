5ec4b5dc9e079983bbba2cda99e9691f5e62c281
Okafor Patrick <patrickpeters911@gmail.com>
2023-09-08 17:32:35 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,80 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-
-#define BUFFER_SIZE 1024
+#include "main.h"
 
 /**
- * main - entry point
- * @argc: the number of arguments
- * @argv: an array of arguments
+ * main - check the code. Entry point
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 0 on success, non-zero on failure
- */
+ * Return: 0 Success
+ **/
+
 int main(int argc, char **argv)
 {
-    int fd_from, fd_to, res;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
+	const char *file_to, *file_from;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        return (EXIT_FAILURE);
-    }
+	file_from = argv[1];
+	file_to = argv[2];
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        return (EXIT_FAILURE);
-    }
-
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (fd_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        close(fd_from);
-        return (EXIT_FAILURE);
-    }
-
-    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written == -1 || bytes_written != bytes_read)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            close(fd_from);
-            close(fd_to);
-            return (EXIT_FAILURE);
-        }
-    }
-
-    if (bytes_read == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        close(fd_from);
-        close(fd_to);
-        return (EXIT_FAILURE);
-    }
-
-    res = close(fd_from);
-    if (res == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        close(fd_to);
-        return (EXIT_FAILURE);
-    }
-
-    res = close(fd_to);
-    if (res == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        return (EXIT_FAILURE);
-    }
-
-    return (EXIT_SUCCESS);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to,\n");
+		exit(97);
+	}
+	copy_file(file_from, file_to);
+	return (0);
 }
 
+/**
+ * copy_file - a function that copies data from one file to another
+ * @file_from: file to read dta for copying
+ * @file_to: file to write data to
+ *
+ * Return: Nothing
+ **/
+
+int copy_file(const char *file_from, const char *file_to)
+{
+	int fd, fd2;
+	char buffer[1024];
+	ssize_t byte_read, byte_written;
+
+	fd2 = open(file_from, O_RDONLY, 0);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	byte_read = read(fd2, buffer, sizeof(buffer));
+	if (byte_read <= 0)
+	{
+		close(fd2);
+		return (-1);
+	}
+	byte_written = write(fd, buffer, _strlen(buffer));
+	if (byte_written <= 0)
+	{
+		close(fd2);
+		return (-1);
+	}
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+		exit(100);
+	}
+	return (1);
+}
+
+/**
+ * _strlen - a function to find the length of a string
+ * @str: the string
+ *
+ * Return: length of string
+ **/
+
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+
+	return (len);
+}
