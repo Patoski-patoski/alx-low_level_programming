6a2efd2367b6514c93a673d7e825a9d07c634279
patrick okafor <codesbypatrick@gmail.com>
2023-09-28 14:19:41 -0700

    a function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+
+
+
+
+#endif
