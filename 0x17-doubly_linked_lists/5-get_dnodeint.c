9e57edef58509bb4a1f7cb2c81546cbce45e4fac
patrick okafor <patrickpeters911@gmail.com>
2023-11-09 21:06:39 -0800

    a function that returns the nth node of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -22,6 +22,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		if (node == index)
 		{
 			current = head;
+			if (!(current->n))
+				return (NULL);
 			return (current);
 		}
 	}
