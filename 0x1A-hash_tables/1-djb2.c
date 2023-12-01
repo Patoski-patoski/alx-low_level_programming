605f4d6e87e70fbdd8166a4def725b7c31fe645b
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 14:03:48 -0800

    add:  a hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2- a function that generates a hash value from a string
+ * @str: strings containing unsigned characters
+ *
+ * Return: a new hash value
+ **/
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
