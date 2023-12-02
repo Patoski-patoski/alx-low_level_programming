03d8015fb7d3d2729058d3f57ebc2bb3a14f5ea1
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 20:32:14 -0800

    fix bug: a function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,23 @@
  *
  **/
 
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	size_t i;
 	hash_table_t *table;
-
-	if (size <= 0)
-		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
-	{
-		/*free(table);*/
 		return (NULL);
-	}
 
-	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
 	return (table);
 }
 
