4f0fd2aef9bf7e72a201cdc7439d949a59b36a26
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 20:37:09 -0800

    fix bug: a function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (node != NULL)
 		{
 			if (comma_flag == 1)
-				printf(", ");
+				printf(",");
 			printf("'%s': '%s' ", node->key, node->value);
 			comma_flag = 1;
 			node = node->next;
