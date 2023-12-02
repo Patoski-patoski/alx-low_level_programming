2b781126f04c11d54930126e759bbd8ff55a3c45
patrick okafor <patrickpeters911@gmail.com>
2023-12-02 09:20:00 -0800

    fix betty bug: a function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,9 +13,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	size_t i;
 	hash_table_t *table;
 
-	if (size < 0 || !table)
-		return NULL;
-
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
