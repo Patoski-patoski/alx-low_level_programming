9d0a551daf16d1339c81a7443753c207235552f4
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 16:15:05 -0800

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,6 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		hash_node_t *current = ht->array[index];
+
 		while (current->next != NULL)
 			current = current->next;
 
