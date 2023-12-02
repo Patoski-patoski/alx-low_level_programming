c4518fe74a38c36cb03f18999371dd60505ecf37
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 16:36:28 -0800

    a function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,40 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *newpair, *current;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return 0;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return 1;
+		}
+		current = current->next;
+	}
+
+	newpair = malloc(sizeof(hash_node_t));
+	if (newpair == NULL)
+		return 0;
+
+	newpair->key = strdup(key);
+	newpair->value = (value != NULL) ? strdup(value) : NULL;
+	newpair->next = ht->array[index];
+	ht->array[index] = newpair;
+
+	return 1;
+}
+/*
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
 	unsigned int index;
 	hash_node_t *newpair = NULL;
 
@@ -47,4 +81,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current->next = newpair;
 	}
 	return (1);
-}
+}*/
