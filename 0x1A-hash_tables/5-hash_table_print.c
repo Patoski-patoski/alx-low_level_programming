0ca5b5859029318be7e66222fa3637cf11d1a3b7
patrick okafor <patrickpeters911@gmail.com>
2023-12-01 19:40:34 -0800

    a function that prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print- a table to print hash table
+ * @ht: the hash table
+ *
+ * Return: nothing
+ *
+ **/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int index;
+	hash_node_t *node;
+	int comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+			printf("'%s': '%s' ", node->key, node->value);
+			comma_flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
